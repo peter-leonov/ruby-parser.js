3ee84924858d01580cb7e3d2ddaa0fef722f7c68
Peter Leonov <gojpeg@gmail.com>
2013-05-29 23:36:19 +0300

    remove Lexer interface

--- lalr1.js
+++ lalr1.js
@@ -80,47 +80,6 @@ b4_token_enums(b4_tokens)
       return new ]b4_location_type[(rhs.locationAt(0).end, rhs.locationAt(0).end);
   }]])[
 
-  /**
-   * Communication interface between the scanner and the Bison-generated
-   * parser <tt>]b4_parser_class_name[</tt>.
-   */
-  public interface Lexer {
-    ]b4_locations_if([[/**
-     * Method to retrieve the beginning position of the last scanned token.
-     * @@return the position at which the last scanned token starts.  */
-    ]b4_position_type[ getStartPos ();
-
-    /**
-     * Method to retrieve the ending position of the last scanned token.
-     * @@return the first position beyond the last scanned token.  */
-    ]b4_position_type[ getEndPos ();]])[
-
-    /**
-     * Method to retrieve the semantic value of the last scanned token.
-     * @@return the semantic value of the last scanned token.  */
-    ]b4_yystype[ getLVal ();
-
-    /**
-     * Entry point for the scanner.  Returns the token identifier corresponding
-     * to the next token and prepares to return the semantic value
-     * ]b4_locations_if([and beginning/ending positions ])[of the token.
-     * @@return the token identifier corresponding to the next token. */
-    int yylex () ]b4_maybe_throws([b4_lex_throws])[;
-
-    /**
-     * Entry point for error reporting.  Emits an error
-     * ]b4_locations_if([referring to the given location ])[in a user-defined way.
-     *
-     * ]b4_locations_if([[@@param loc The location of the element to which the
-     *                error message is related]])[
-     * @@param s The string for the error message.  */
-     void yyerror (]b4_locations_if([b4_location_type[ loc, ]])[String s);]
-  }
-
-  b4_lexer_if([[private class YYLexer implements Lexer {
-]b4_percent_code_get([[lexer]])[
-  }
-
   ]])[/** The object doing lexical analysis for us.  */
   private Lexer yylexer;
   ]
