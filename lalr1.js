db329a9e713801b241900a63459d8e36eb83cb71
Peter Leonov <gojpeg@gmail.com>
2013-05-29 19:04:46 +0300

    hope best

--- lalr1.js
+++ lalr1.js
@@ -1,6 +1,6 @@
-# Java skeleton for Bison -*- autoconf -*-
+# JavaScript skeleton for Bison -*- autoconf -*-
 
-# Copyright (C) 2007-2013 Free Software Foundation, Inc.
+# Copyright (C) 2013 Free Software Foundation, Inc.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
