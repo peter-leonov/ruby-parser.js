192356d3b9d3b3ad5bc85b2f4fd1faf9068b0d60
Peter Leonov <gojpeg@gmail.com>
2013-06-09 22:06:43 +0300

    g c

--- lexer.js
+++ lexer.js
@@ -1,146 +1,157 @@
 "use strict";
 
+var tSTRING_END = 300,
+    tREGEXP_END = 301
+
 ;(function(){
 
-function create_lexer (text)
+// $stream: plain old JS string with ruby source code
+function Lexer ($stream)
 {
 
-var parser =
+// the lex() method and all public data sit here
+var lexer = this;
+// the end of stream had been reached
+lexer.eofp = false;
+// the string to be parsed in the nex lex() call
+lexer.strterm = null;
+// the main point of interaction with the parser out there
+lexer.state = null;
+
+// ignore newline, +/- is a sign.
+var EXPR_BEG    = 1 << 0;
+// newline significant, +/- is an operator.
+var EXPR_END    = 1 << 1;
+// ditto, and unbound braces.
+var EXPR_ENDARG = 1 << 2;
+// ditto, and unbound braces.
+var EXPR_ENDFN  = 1 << 3;
+// newline significant, +/- is an operator.
+var EXPR_ARG    = 1 << 4;
+// newline significant, +/- is an operator.
+var EXPR_CMDARG = 1 << 5;
+// newline significant, +/- is an operator.
+var EXPR_MID    = 1 << 6;
+// ignore newline, no reserved words.
+var EXPR_FNAME  = 1 << 7;
+// right after `.' or `::', no reserved words.
+var EXPR_DOT    = 1 << 8;
+// immediate after `class', no here document.
+var EXPR_CLASS  = 1 << 9;
+// alike EXPR_BEG but label is disallowed.
+var EXPR_VALUE  = 1 << 10;
+
+var EXPR_BEG_ANY = EXPR_BEG | EXPR_VALUE | EXPR_MID | EXPR_CLASS;
+var EXPR_ARG_ANY = EXPR_ARG | EXPR_CMDARG;
+var EXPR_END_ANY = EXPR_END | EXPR_ENDARG | EXPR_ENDFN;
+
+
+
+
+
+// here go all $strem related functions
+
+var $streamLength = $stream.length;
+var $pos = 0;
+
+// just an emulation of pos[i] from C
+function nthchar (i)
 {
-  eofp: false,
-  line_count: 0
-};
-
-var lex_strterm = 0;
-var lex_state = 0;
-var parser_cond_stack = 0;
-var parser_cmdarg_stack = 0;
-var parser_class_nest = 0;
-var parser_paren_nest = 0;
-var parser_lpar_beg = 0;
-var parser_brace_nest = 0;
-var parser_in_single = 0;
-var parser_in_def = 0;
-var parser_in_defined = 0;
-var parser_compile_for_eval = 0;
-var parser_cur_mid = 0;
-var parser_tokenbuf = null;
-var parser_tokidx = 0;
-var parser_toksiz = 0;
-var heredoc_end = 0;
-var command_start = true;
-var parser_deferred_nodes = 0;
-var lex_pbeg = 0;
-var lex_p = 0;
-var lex_pend = 0;
-var parser_lvtbl = 0;
-var parser_ruby__end__seen = 0;
-var parser_ruby_sourcefile = ''; /* current source file */
-var is_ripper = 0;
-var parser_eval_tree_begin = 0;
-var parser_eval_tree = 0;
-
-var lex_input = 0;
-var lex_nextline = 0;
-var ruby_sourceline = 0; /* current line no. */
-
-
-function lex_p_get (i)
-{
-  return text[lex_p + i];
+  return $stream[$pos + i];
 }
 
-function lex_goto_eol () { return lex_p = lex_pend }
+// search for `\n` and stop right after it,
+// if the position of `\n` been found at 3: "abc|\n"
+// then `$pos` will be 4: "abc\n|"
+function lex_goto_eol ()
+{
+  while (nextc() != '\n');
+}
 
+// forecast, if the nextc() will return character `c`
 function peek (c)
 {
-  return peek_n(c, 0)
+  return $pos < $streamLength && c == $stream[$pos];
 }
 
+// forecast, if the nextc() will return character `c`
+// after n calls
 function peek_n (c, n)
 {
-  return lex_p + n < lex_pend && c == lex_p_get(n)
+  var pos = $pos + n;
+  return pos < $streamLength && c == $stream[pos];
 }
 
+// returns next character from the `$stream`,
+// or an empty string '' if there is no more characters
 function nextc ()
 {
-  if (lex_p == lex_pend)
+  if ($pos >= $streamLength)
   {
-      if (parser.eofp)
-        return '';
-
-      parser.eofp = true;
-      lex_goto_eol();
-      return '';
+    lexer.eofp = true;
+    return '';
   }
-  var c = text[lex_p++];
-  if (c == '\r' && peek('\n'))
-  {
-    lex_p++;
-    c = '\n';
-  }
-
-  return c;
+  
+  return $stream[$pos++];
 }
 
-
+// step back for one character and check
+// if the current character is equal to `c`
 function pushback (c)
 {
   if (c == '')
-    return;
-  
-  lex_p--;
-  if (lex_p > lex_pbeg && lex_p_get(0) == '\n' && lex_p_get(-1) == '\r')
   {
-    lex_p--;
+    if ($pos != $streamLength)
+      throw 'lexer error: pushing back wrong EOF char';
+    return;
   }
+  
+  $pos--;
+  if ($stream[$pos] != c)
+    throw 'lexer error: pushing back wrong "'+c+'" char';
 }
 
-
-lex_pend = text.length
-
-return function parser_yylex ()
+this.lex = function yylex ()
 {
-  var c = ''
-  var space_seen = false
+  var c = '';
+  var space_seen = false;
   
-  // if (lex_strterm)
-  // {
-  //   int token;
-  //   if (nd_type(lex_strterm) == NODE_HEREDOC)
-  //   {
-  //     token = here_document(lex_strterm);
-  //     if (token == tSTRING_END)
-  //     {
-  //       lex_strterm = 0;
-  //       lex_state = EXPR_END;
-  //     }
-  //   }
-  //   else
-  //   {
-  //     token = parse_string(lex_strterm);
-  //     if (token == tSTRING_END || token == tREGEXP_END)
-  //     {
-  //       rb_gc_force_recycle((VALUE) lex_strterm);
-  //       lex_strterm = 0;
-  //       lex_state = EXPR_END;
-  //     }
-  //   }
-  //   return token;
-  // }
+  if (lexer.strterm)
+  {
+    var token = 0;
+    if (lexer.strterm.type == 'HEREDOC')
+    {
+      token = here_document(lexer.strterm);
+      if (token == tSTRING_END)
+      {
+        lexer.strterm = null;
+        lexer.state = EXPR_END;
+      }
+    }
+    else
+    {
+      token = parse_string(lexer.strterm);
+      if (token == tSTRING_END || token == tREGEXP_END)
+      {
+        lexer.strterm = null;
+        lex_state = EXPR_END;
+      }
+    }
+    return token;
+  }
   
-  var cmd_state = command_start;
-  command_start = false;
+  // var cmd_state = command_start;
+  // command_start = false;
   
   retry: for (;;)
   {
-  var last_state = lex_state;
+  var last_state = lexer.strterm;
   switch (c = nextc())
   {
     case '\0':                 /* NUL */
     case '\x04':               /* ^D */
     case '\x1a':               /* ^Z */
-    case -1:                   /* end of script. */
+    case '':                   /* end of script. */
       return 0;
     
       /* white spaces */
@@ -149,7 +160,7 @@ return function parser_yylex ()
     case '\f':
     case '\r':
     case '\v':                /* '\13' */
-      space_seen = 1;
+      space_seen = true;
       continue retry;
     
   }
@@ -163,10 +174,10 @@ return function parser_yylex ()
 
 function lex_all (text)
 {
-  var lexer = create_lexer(text)
+  var lexer = new Lexer(text)
   
   var t = 0
-  while (t = lexer())
+  while (t = lexer.lex())
     print(t)
 }
 
