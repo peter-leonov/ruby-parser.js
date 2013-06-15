5e5bb20817ff7795854757e1fc45bef9428ed082
Peter Leonov <gojpeg@gmail.com>
2013-06-15 04:15:48 +0300

    the $

--- lexer.js
+++ lexer.js
@@ -1343,6 +1343,120 @@ this.yylex = function yylex ()
       return $('%');
     }
     
+    case '$':
+    {
+      lexer.lex_state = EXPR_END;
+      newtok();
+      c = nextc();
+      switch (c)
+      {
+        case '_':              /* $_: last read line string */
+          c = nextc();
+          if (parser_is_identchar(c))
+          {
+            tokadd('$');
+            tokadd('_');
+            break;
+          }
+          pushback(c);
+          c = '_';
+          /* fall through */
+        case '~':              /* $~: match-data */
+        case '*':              /* $*: argv */
+        case '$':              /* $$: pid */
+        case '?':              /* $?: last status */
+        case '!':              /* $!: error string */
+        case '@':              /* $@: error position */
+        case '/':              /* $/: input record separator */
+        case '\\':             /* $\: output record separator */
+        case ';':              /* $;: field separator */
+        case ',':              /* $,: output field separator */
+        case '.':              /* $.: last read line number */
+        case '=':              /* $=: ignorecase */
+        case ':':              /* $:: load path */
+        case '<':              /* $<: reading filename */
+        case '>':              /* $>: default output handle */
+        case '\"':             /* $": already loaded files */
+          tokadd('$');
+          tokadd(c);
+          tokfix();
+          // set_yylval_name(rb_intern(tok())); TODO
+          return tGVAR;
+
+        case '-':
+          tokadd('$');
+          tokadd(c);
+          c = nextc();
+          if (parser_is_identchar(c))
+          {
+            if (tokadd(c) == '')
+              return 0;
+          }
+          else
+          {
+            pushback(c);
+          }
+        // was: gvar:
+          tokfix();
+          // set_yylval_name(rb_intern(tok())); TODO
+          return tGVAR;
+
+        case '&':              /* $&: last match */
+        case '`':              /* $`: string before last match */
+        case '\'':             /* $': string after last match */
+        case '+':              /* $+: string matches last paren. */
+          if (IS_lex_state_for(lexer.last_state, EXPR_FNAME))
+          {
+            tokadd('$');
+            tokadd(c);
+            // was: goto gvar;
+            tokfix();
+            // set_yylval_name(rb_intern(tok())); TODO
+            return tGVAR;
+          }
+          // set_yylval_node(NEW_BACK_REF(c)); TODO
+          return tBACK_REF;
+
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          tokadd('$');
+          do
+          {
+            tokadd(c);
+            c = nextc();
+          }
+          while (c != '' && ISDIGIT(c));
+          pushback(c);
+          if (IS_lex_state_for(lexer.last_state, EXPR_FNAME))
+          {
+            // was: goto gvar;
+            tokfix();
+            // set_yylval_name(rb_intern(tok())); TODO
+            return tGVAR;
+          }
+          tokfix();
+          // set_yylval_node(NEW_NTH_REF(atoi(tok() + 1))); TODO
+          return tNTH_REF;
+
+        default:
+          if (!parser_is_identchar(c))
+          {
+            pushback(c);
+            return $('$');
+          }
+        case '0':
+          tokadd('$');
+      }
+      break;
+    }
+    
     // add before here :)
     
     default:
