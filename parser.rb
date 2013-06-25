fd1aa4e57c76a05ed4056b963b813776cd542077
Peter Leonov <gojpeg@gmail.com>
2013-06-26 01:54:14 +0300

    print warnings

--- parser.rb
+++ parser.rb
@@ -1,2 +1,11 @@
 require 'parser/ruby20'
-p Parser::Ruby20.parse(File.read("ruby.rb"))
+
+parser = Parser::Ruby20.new
+parser.diagnostics.consumer = lambda do |diag|
+  puts diag.render
+end
+
+buffer = Parser::Source::Buffer.new('ruby.rb')
+buffer.source = File.read("ruby.rb")
+
+p parser.parse(buffer)
