098c79198fadf3f83fae5bed9ddf985c35bdeebf
Peter Leonov <gojpeg@gmail.com>
2013-06-26 02:07:52 +0300

    more on inspection

--- builder-node.js
+++ builder-node.js
@@ -1,3 +1,8 @@
+function inspect (v)
+{
+  return (v && v.inspect) ? v.inspect() : JSON.stringify(v);
+}
+
 function Node (type, children)
 {
   this.type = type;
@@ -5,24 +10,31 @@ function Node (type, children)
 }
 Node.prototype.inspect = function ()
 {
-  var pairs = [];
-  for (var k in this)
-  {
-    if (k == 'type' || k == 'inspect' || k == 'line')
-      continue;
-    var v = this[k];
-    v = (v && v.inspect) ? v.inspect() : JSON.stringify(v);
-    pairs.push(k + ': ' + v);
-  }
+  var children = this.children;
   
-  return this.type + '(' + pairs.join(', ') + ')';
+  var parts = [];
+  for (var i = 0, il = children.length; i < il; i++)
+    parts[i] = inspect(children[i]);
+  
+  return this.type + '(' + parts.join(', ') + ')';
 }
+
 function n (type, children)
 {
   return new Node(type, children);
 }
+
+
 function array (ary)
 {
-  ary.inspect = function () { return 'array['+this.length+']' } // TODO
+  ary.inspect = array_inspect;
   return ary;
+}
+function array_inspect ()
+{
+  var parts = [];
+  for (var i = 0, il = this.length; i < il; i++)
+    parts[i] = inspect(this[i]);
+  
+  return '[' + parts.join(', ') + ']';
 }
