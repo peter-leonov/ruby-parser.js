77629330a719fdd0a5102c9ce33195b91ff6dc75
Peter Leonov <gojpeg@gmail.com>
2013-07-01 01:36:48 +0300

    emits json

---
+++ ruby2json.js
@@ -0,0 +1,53 @@
+var util = require('util');
+var fs   = require('fs');
+
+global.print = function ()
+{
+  var ary = Array.prototype.slice.apply(arguments);
+  util.print(ary.join(' '), '\n');
+}
+
+global.write = function ()
+{
+  var ary = Array.prototype.slice.apply(arguments);
+  util.print(ary.join(' '));
+}
+
+
+var Parser = require('./parse.js');
+
+
+function parse (text)
+{
+  var lexer = new Parser.YYLexer(text);
+  lexer.filename = 'ruby.rb';
+  
+  var parser = new Parser.YYParser(lexer);
+  parser.yydebug = 1; // render all the states transitions
+  parser.yydebug_yylval = true; // don't print token values
+  // parser.yydebug_action = true; // print actions applied
+  var ok = parser.parse();
+  return {ok: ok, ast: parser.resulting_ast};
+}
+
+var text = fs.readFileSync('ruby.rb', {encoding: 'utf8'});
+var result = parse(text);
+
+function to_plain (n)
+{
+  if (!(n && n.type))
+    return n;
+    
+  var ary = n.slice();
+  ary.unshift(n.type);
+  
+  for (var i = 0, il = ary.length; i < il; i++)
+    ary[i] = to_plain(ary[i]);
+  
+  return ary;
+}
+
+write(JSON.stringify(to_plain(result.ast)));
+
+process.exit(+!result.ok);
+
