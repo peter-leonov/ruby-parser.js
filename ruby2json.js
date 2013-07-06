f84290a3181402d2a6a45409f31eb2f72dd7c8dd
Peter Leonov <gojpeg@gmail.com>
2013-07-06 04:12:05 +0300

    update node demo

--- ruby2json.js
+++ ruby2json.js
@@ -14,42 +14,16 @@ global.write = function ()
 }
 
 
-var Parser = require('./parse.js');
+var RubyParser = require('./parse.js').RubyParser;
+
+var parser = new RubyParser();
 
 var rbfile = process.argv[2];
-
-
-function parse (text, filename)
-{
-  var lexer = new Parser.YYLexer(text);
-  lexer.filename = filename;
-  
-  var parser = new Parser.YYParser(lexer);
-  parser.yydebug = 1; // render all the states transitions
-  parser.yydebug_yylval = true; // don't print token values
-  // parser.yydebug_action = true; // print actions applied
-  var ok = parser.parse();
-  return {ok: ok, ast: parser.resulting_ast};
-}
-
 var text = fs.readFileSync(rbfile, {encoding: 'utf8'});
-var result = parse(text, rbfile);
 
-function to_plain (n)
-{
-  if (!(n && n.type))
-    return n;
-    
-  var ary = n.slice();
-  ary.unshift(n.type);
-  
-  for (var i = 0, il = ary.length; i < il; i++)
-    ary[i] = to_plain(ary[i]);
-  
-  return ary;
-}
+var json = parser.toJSON(text, rbfile);
 
-write(JSON.stringify(to_plain(result.ast)));
+print(json);
 
-process.exit(+!result.ok);
+process.exit(json ? 0 : 1);
 
