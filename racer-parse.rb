2bd5217112a5d0aa9baae484a7e6efbe36019ead
Peter Leonov <gojpeg@gmail.com>
2013-07-06 03:40:20 +0300

    all this dramatically simplifies the racer wrapper

--- racer-parse.rb
+++ racer-parse.rb
@@ -23,73 +23,18 @@ class ParserJS
   def initialize
     load_parser
     
-    @parser = @@js.eval <<-JS
-      (function (){
-        var lexer = new YYLexer();
-        lexer.filename = '(source)';
-  
-        var parser = new YYParser(lexer);
-
-        function to_plain (n)
-        {
-          if (!(n && n.type))
-            return n;
-
-          var ary = n.slice();
-          ary.unshift(n.type);
-
-          for (var i = 0, il = ary.length; i < il; i++)
-            ary[i] = to_plain(ary[i]);
-
-          return ary;
-        }
-
-        function reset ()
-        {
-          lexer.reset();
-        }
-
-        function declare (v)
-        {
-          parser.declareVar(v);
-        }
-      
-        function set_filename (fn)
-        {
-          lexer.filename = fn;
-        }
-      
-        function give_me_json (ruby)
-        {
-          lexer.setText(ruby);
-          var ok = parser.parse(ruby);
-          return JSON.stringify(to_plain(parser.resulting_ast));
-        }
-        
-        return {
-          reset: reset,
-          set_filename: set_filename,
-          declare: declare,
-          give_me_json: give_me_json,
-          
-        }
-      })()
-    JS
+    @parser = @@js.eval('new RubyParser()')
   end
   
   def parse source
-    @parser["give_me_json"].call(source)
+    @parser.toJSON(source)
   end
   
   def declare var
-    @parser["declare"].call(var)
+    @parser.declareVar(var)
   end
   
   def filename= name
-    @parser["set_filename"].call(name)
-  end
-  
-  def reset_lexer
-    @parser["reset"].call
+    @parser.setFilename(name)
   end
 end
