ce96d4c318e312097a2f7c153e042adeaf28a281
Peter Leonov <gojpeg@gmail.com>
2013-07-24 19:34:20 +0300

    all the code for the locations burden

--- spec/test_locations.spec.js
+++ spec/test_locations.spec.js
@@ -16,13 +16,42 @@ function s ()
   return slice.call(arguments);
 }
 
+global.print = require('util').print;
+
 // the main workhorse
 
-function assert_parses (ast, code)
+function unpack_float_loc (n)
 {
-  var a = JSON.stringify(ast);
-  var b = JSON.stringify(RubyParser.Builder.toPlain(parser.parse(code, '(assert_parses)')));
-  expect(b).toBe(a);
+  var integer = n | 0;
+  var decimal = n - integer;
+
+  if (decimal <= 0.9)
+    decimal *= 10
+  else if (decimal <= 0.99)
+    decimal *= 100
+  else // if decimal > 0.99)
+    decimal = (decimal * 1000) | 0
+
+  return {line: integer, col: decimal};
+}
+
+function assert_location (loc, code)
+{
+  // do actual parsing
+  var node = parser.parse(code, '(assert_parses)');
+
+  // find out and unpack the root node location
+  var node_loc;
+  if (node.loc == undefined)
+    node_loc = undefined;
+  else
+    node_loc = RubyParser.Lexer.unpack_location(node.loc);
+
+  // unpack cosy float-encoded location
+  var expected = unpack_float_loc(loc);
+
+  // copare their unpacked values (two hashes)
+  expect(node_loc).toBe(expected);
 }
 
 
